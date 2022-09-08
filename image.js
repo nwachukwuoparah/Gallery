dcdb8be722224ff5132533acc4d066675373d487
Oparah <nwachukwuoparah@gmail.com>
2022-09-08 20:20:41 +0100

    commit

---
+++ image.js
@@ -0,0 +1,30 @@
+function prepareGallery() {
+  if (!document.getElementsByTagName) return false;
+  if (!document.getElementById) return false;
+  if (!document.getElementById("imagegallery")) return false;
+  var gallery = document.getElementById("imagegallery");
+  var links = gallery.getElementsByTagName("a");
+  for (var i = 0; i < links.length; i++) {
+    links[i].onclick = function () {
+      return showPic(this) ? false : true;
+    };
+  }
+}
+window.onload = prepareGallery;
+function showPic(whichpic) {
+  if (!document.getElementById("placeholder")) return false;
+  var source = whichpic.getAttribute("href");
+  var placeholder = document.getElementById("placeholder");
+  if (placeholder.nodeName != "IMG") return false;
+  placeholder.setAttribute("src", source);
+  if (document.getElementById("description")) {
+    var text = whichpic.getAttribute("title")
+      ? whichpic.getAttribute("title")
+      : "";
+    var description = document.getElementById("description");
+    if (description.firstChild.nodeType == 3) {
+      description.firstChild.nodeValue = text;
+    }
+  }
+  return true;
+}
