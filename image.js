60e1a479d6aeba37b41f3c1f4660f89b55833ba8
Oparah <nwachukwuoparah@gmail.com>
2022-09-14 02:14:38 +0100

    marl up on the fly

--- image.js
+++ image.js
@@ -1,30 +1,72 @@
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != "function") {
+    window.onload = func;
+  } else {
+    window.onload = function () {
+      oldonload();
+      func();
+    };
+  }
+}
+function insertAfter(newElement, targetElement) {
+  let parent = targetElement.parentNode;
+  if (parent.lastChild == targetElement) {
+    parent.appendChild(newElement);
+  } else {
+    parent.insertBefore(newElement, targetElement.nextSibling);
+  }
+}
+
+function preparePlaceholder() {
+  if (!document.createElement) return false;
+  if (!document.createTextNode) return false;
+  if (!document.getElementById) return false;
+  if (!document.getElementById("imagegallery")) return false;
+  let placeholder = document.createElement("img");
+  placeholder.setAttribute("id", "placeholder");
+  placeholder.setAttribute("src", "images/placeholder.gif");
+  placeholder.setAttribute("alt", "my image gallery");
+  let description = document.createElement("p");
+  description.setAttribute("id", "description");
+  let desctext = document.createTextNode("Choose an image");
+  description.appendChild(desctext);
+  let gallery = document.getElementById("imagegallery");
+  insertAfter(placeholder, gallery);
+  insertAfter(description, placeholder);
+}
+
 function prepareGallery() {
   if (!document.getElementsByTagName) return false;
   if (!document.getElementById) return false;
   if (!document.getElementById("imagegallery")) return false;
-  var gallery = document.getElementById("imagegallery");
-  var links = gallery.getElementsByTagName("a");
-  for (var i = 0; i < links.length; i++) {
+  let gallery = document.getElementById("imagegallery");
+  let links = gallery.getElementsByTagName("a");
+  for (let i = 0; i < links.length; i++) {
     links[i].onclick = function () {
       return showPic(this) ? false : true;
     };
+    links[i].onkeypress = links[i].onclick;
   }
 }
-window.onload = prepareGallery;
+
+// window.onload = prepareGallery;
 function showPic(whichpic) {
   if (!document.getElementById("placeholder")) return false;
-  var source = whichpic.getAttribute("href");
-  var placeholder = document.getElementById("placeholder");
+  let source = whichpic.href;
+  let placeholder = document.getElementById("placeholder");
   if (placeholder.nodeName != "IMG") return false;
-  placeholder.setAttribute("src", source);
+  placeholder.src = source;
   if (document.getElementById("description")) {
-    var text = whichpic.getAttribute("title")
+    let text = whichpic.getAttribute("title")
       ? whichpic.getAttribute("title")
       : "";
-    var description = document.getElementById("description");
+    let description = document.getElementById("description");
     if (description.firstChild.nodeType == 3) {
       description.firstChild.nodeValue = text;
     }
   }
   return true;
 }
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
